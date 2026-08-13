790ced7059f5b4eabae3c7ede775e9e030aa7dda
riyadx69x <skriadhossenbk@gmail.com>
2026-08-14 00:30:13 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import json
 import time
 
 api_key = 'sk_live_1x7jN6OUqTIzUNEv7MIM9Er2h5GphCXer9ef4BUx'
-BOT_TOKEN = "8564093311:AAH55oqI6UmMfXycsEtxtIMjOHNN6atuVoo"
+BOT_TOKEN = "8564093311:AAE1wtnRDybV4oOH3HgmJbHplsBovYVtZm8"
 CHAT_ID = "-1003178872820"
 
 url = 'https://redxsms.com/api/v1/iprn/messages'
